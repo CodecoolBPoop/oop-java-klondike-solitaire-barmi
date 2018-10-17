411a02d6a52dec319736c49d63adc8afee187bb1
Gábor Kovács <gabor950601@gmail.com>
2018-10-17 13:18:09 +0200

    nothing just newlines

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -56,7 +56,12 @@ public class Game extends Pane {
 
     private EventHandler<MouseEvent> onMouseDraggedHandler = e -> {
         Card card = (Card) e.getSource();
+
         Pile activePile = card.getContainingPile();
+
+        List<Card> cards = activePile.getCards();
+
+
         if (activePile.getPileType() == Pile.PileType.STOCK)
             return;
         double offsetX = e.getSceneX() - dragStartX;
